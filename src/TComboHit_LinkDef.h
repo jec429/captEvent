63c3cbb27f5ece0ee3d8260c8d6093cd86dd4023
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-05-03 09:36:27 -0400

    Remove THit derived classes that will not be used.
    These are old THit derived classes that were not found to be useful for the
    ND280.  They mostly duplicate functionality in TReconHit.  The TMultiHit
    object is the most useful since it can be used to replicate a FADC signal,
    but in ND280 we found that the FADC pulse could be easily processed at the
    digit level and the hits could just represent the final pulse positions,
    times, &c.  In anycase, the current TMultiHit object was not optimal for
    the problem it tried to solve.

--- src/TComboHit_LinkDef.h
+++
@@ -1,5 +0,0 @@
-#ifdef __CINT__
-#pragma link C++ class CP::TComboHit+;
-#pragma link C++ class CP::THandle<CP::TComboHit>+;
-#endif
-
--- src/TMultiHit_LinkDef.h
+++
@@ -1,6 +0,0 @@
-#ifdef __CINT__
-#pragma link C++ class CP::TMultiHit+;
-#pragma link C++ class CP::THandle<CP::TMultiHit>+;
-#endif
-
-
