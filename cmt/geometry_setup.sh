449b123b82e775057f626e7f6d6254428b5d76b0
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-07-24 09:28:46 -0400

    Handle multiple versions of geometry files.
    The geometry_setup script picks the default geometry files for CAPTAIN and
    miniCAPTAIN, but was not correctly handling the situation when there were
    several possible files.  The script now picks the highest version number.

--- cmt/geometry_setup.sh
+++ cmt/geometry_setup.sh
@@ -4,8 +4,8 @@
 ${CAPTEVENTROOT}/scripts/captain-get-geometry
 
 export CAPTAINGEOMETRY
-CAPTAINGEOMETRY=`find ${CAPTEVENTROOT}/${CAPTEVENTCONFIG} -name "*baseline*"`
+CAPTAINGEOMETRY=`find ${CAPTEVENTROOT}/${CAPTEVENTCONFIG} -name "*baseline*" | sort -V | tail -1`
 
 export MINICAPTAINGEOMETRY
-MINICAPTAINGEOMETRY=`find ${CAPTEVENTROOT}/${CAPTEVENTCONFIG} -name "*mini-captain*"`
+MINICAPTAINGEOMETRY=`find ${CAPTEVENTROOT}/${CAPTEVENTCONFIG} -name "*mini-captain*" | sort -V | tail -1`
 
