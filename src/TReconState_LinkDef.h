98e8b3fb61d331b2e6062099ff9cebc15e4bd787
Clark McGrew <clark.mcgrew@stonybrook.edu>
2016-03-20 03:56:00 +0900

    Correct the TReconState_LinkDef.h for newer roots.
    This removes the rootcint definitions for the TM<>State mix-in classes
    which should never be saved in an output file (or read from an input
    file).  The previous code was how to do it for older root versions.

--- src/TReconState_LinkDef.h
+++ src/TReconState_LinkDef.h
@@ -3,16 +3,4 @@
 #pragma link C++ class CP::TReconState+;
 #pragma link C++ class CP::THandle<CP::TReconState>+;
 
-#pragma link C++ class CP::TMReconState-;
-#pragma link C++ class CP::TMEDepositState-;
-#pragma link C++ class CP::TMPositionState-;
-#pragma link C++ class CP::TMDirectionState-;
-#pragma link C++ class CP::TMPositionDirectionState-;
-#pragma link C++ class CP::TMConeState-;
-#pragma link C++ class CP::TMMassState-;
-#pragma link C++ class CP::TMPosDirMassState-;
-#pragma link C++ class CP::TMWidthState-;
-#pragma link C++ class CP::TMMomentumState-;
-#pragma link C++ class CP::TMChargeState-;
-
 #endif
