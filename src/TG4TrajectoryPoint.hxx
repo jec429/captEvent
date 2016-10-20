575a4b7562a91c6dc4cfc3b1c7fed05647886c86
Clark McGrew <clark.mcgrew@stonybrook.edu>
2016-10-20 15:55:28 -0400

    Documentation improvements for TG4TrajectoryPoint.

--- src/TG4TrajectoryPoint.hxx
+++ src/TG4TrajectoryPoint.hxx
@@ -17,7 +17,9 @@ namespace CP {
 /// A class to save a G4 trajectory point into a root output file without
 /// linking to geant.  The trajectory point is saved in a TG4Trajectory as a
 /// way to record the path of a particle through the detector.  This is the
-/// truth information about the particles which were tracked.
+/// truth information about the particles which were tracked, but is not a
+/// good record of the energy deposition.  Use the TG4HitSegment objects for a
+/// record of the energy deposition.
 class CP::TG4TrajectoryPoint: public TObject {
 public:
     TG4TrajectoryPoint();
@@ -61,35 +63,34 @@ public:
         return fVolumeNode;
     }
 
-    /// Set the G4 process type and and subtype.
-    void SetProcessType(int type, int subtype) {
-        fProcessType = type + 1000*subtype;
-    }
-
-    /// Process types copied from the G4 definitions so that we don't directly
-    /// depend on GEANT4 being installed to read the data.  The exact
-    /// definitions are in the GEANT documentation, but the (important) names
-    /// are pretty self explanatory.
-    enum {
-        kProcessNotDefined,
-        kProcessTransportation,
-        kProcessElectromagetic,
-        kProcessOptical,
-        kProcessHadronic,
-        kProcessPhotoLeptonHadron,
-        kProcessDecay,
-        kProcessGeneral,
-        kProcessParameterization,
-        kProcessUserDefined
+    /// Process types copied from the G4 definitions so that this can be
+    /// compiled without having geant4 installed.  Check the exact definitions
+    /// are in the geant4 documentation, but the (important) names are pretty
+    /// self explanatory.  The definitions can be found in the geant4 include
+    /// file named G4ProcessType.hh.
+    enum G4ProcessType {
+        kProcessNotDefined = 0,
+        kProcessTransportation = 1,
+        kProcessElectromagetic = 2 ,
+        kProcessOptical = 3,
+        kProcessHadronic = 4,
+        kProcessPhotoLeptonHadron = 5,
+        kProcessDecay = 6,
+        kProcessGeneral = 7,
+        kProcessParameterization = 8,
+        kProcessUserDefined = 9
     };
     
     /// Get the G4ProcessType (as an integer).
     int GetProcessType() const {return fProcessType % 1000;}
 
-    /// Several important process sub-types as defined by GEANT4.  These are
+    /// Several important process sub-types as defined by geant4.  These are
     /// copied so that reading the files does not directly depend on having
-    /// GEANT installed.
-    enum {
+    /// geant4 installed.  Check the geant4 documentation for the
+    /// documentation, but most of the names are fairly self explanatory.  The
+    /// definitions are mostly found in the geant4 include files named
+    /// G4HadronicProcessType.hh and G4EmProcessSubType.hh
+    enum G4ProcessSubtype {
         // EM subtypes for charged particles.
         kSubtypeEMCoulombScattering = 1,
         kSubtypeEMIonization = 2,
@@ -115,6 +116,19 @@ public:
     
     /// Get the G4 process subtype (as an integer).
     int GetProcessSubtype() const {return fProcessType / 1000;}
+
+    /// Set the G4 process type and and subtype (this is low level "developer"
+    /// information).  The type and sub-type can be set directly from the
+    /// value of
+    /// aStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessType()
+    /// and
+    /// aStep->GetPostStepPoint()->GetProcessDefinedStep()->GetProcessSubType()
+    /// which made available in DETSIM by
+    /// DSimTrajectoryPoint::GetProcessType() and
+    /// DSimTrajectoryPoint::GetProcessSubType().
+    void SetProcessType(int type, int subtype) {
+        fProcessType = type + 1000*subtype;
+    }
 
     /// Print the hit information.
     virtual void ls(Option_t * = "") const;
